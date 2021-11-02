017084b69d14092e81d58fc352f7ba8343aa8ce7
RuslanAgishev <agishrus@fel.cvut.cz>
2021-11-02 18:14:24 +0100

    tested depth_cloud

--- src/depth_correction/depth_cloud.py
+++ src/depth_correction/depth_cloud.py
@@ -6,6 +6,26 @@ __all__ = [
 ]
 
 
+def depth_cloud_from_points(pts, vps=None):
+    """Create depth cloud from points and viewpoints.
+
+    :param pts: Points as ...-by-3 tensor.
+    :param vps: Viewpoints as ...-by-3 tensor, or None for zero vector.
+    :return:
+    """
+    assert isinstance(pts, torch.Tensor)
+    if vps is None:
+        vps = torch.zeros((1, 3))
+    assert isinstance(vps, torch.Tensor)
+    assert vps.shape == pts.shape or vps.shape == (1, 3)
+    dirs = pts - vps
+    depth = dirs.norm(dim=-1).unsqueeze(-1)
+    # TODO: Handle invalid points (zero depth).
+    dirs = dirs / depth
+    depth_cloud = DepthCloud(vps, dirs, depth)
+    return depth_cloud
+
+
 class DepthCloud(object):
 
     def __init__(self, vps=None, dirs=None, depth=None):
@@ -16,13 +36,13 @@ class DepthCloud(object):
         :param depth: Depth map as ...-by-1 tensor.
         """
         if vps is None:
-            vps = torch.zeros((3,))
+            vps = torch.zeros((1, 3))
         assert isinstance(vps, torch.Tensor)
         assert vps.shape[-1] == 3
 
         assert isinstance(dirs, torch.Tensor)
         assert dirs.shape[-1] == 3
-        assert dirs.shape == vps.shape or vps.shape == [3]
+        assert dirs.shape == vps.shape or vps.shape == (1, 3)
 
         assert isinstance(depth, torch.Tensor)
         assert depth.shape[-1] == 1
@@ -36,22 +56,13 @@ class DepthCloud(object):
         pts = self.vps + self.depth * self.dirs
         return pts
 
-    @staticmethod
-    def from_points(pts, vps=None):
-        """Create depth cloud from points and viewpoints.
-
-        :param pts: Points as ...-by-3 tensor.
-        :param vps: Viewpoints as ...-by-3 tensor, or None for zero vector.
-        :return:
-        """
-        assert isinstance(pts, torch.Tensor)
-        if vps is None:
-            vps = torch.zeros((3,))
-        assert isinstance(vps, torch.Tensor)
-        assert vps.shape == pts.shape or vps.shape == [3]
-        dirs = pts - vps
-        depth = dirs.norm(dim=-1)
-        # TODO: Handle invalid points (zero depth).
-        dirs = dirs / depth
-        depth_cloud = DepthCloud(vps, dirs, depth)
-        return depth_cloud
+    def visualize(self):
+        try:
+            import open3d as o3d
+        except:
+            print('Install Open3d for visualization')
+            return
+        cloud = self.to_points()
+        pcd = o3d.geometry.PointCloud()
+        pcd.points = o3d.utility.Vector3dVector(cloud)
+        o3d.visualization.draw_geometries([pcd])

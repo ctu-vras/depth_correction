0afa4c5b0e81948eb871f6b21bf14afcae15e903
Tomas Petricek <tpetricek@gmail.com>
2022-02-01 14:18:56 +0100

    Using refactored loss params in optimization.

--- scripts/optimization.py
+++ scripts/optimization.py
@@ -95,7 +95,7 @@ def main():
         # combined = model(combined)
         # combined.update_all(r=r_nn, k=k_nn)
 
-        loss, loss_dc = min_eigval_loss(combined, r=r_nn, k=k_nn, offset=True, updated_eigval_bounds=(0.0, 0.05 ** 2))
+        loss, loss_dc = min_eigval_loss(combined, r=r_nn, k=k_nn, offset=True, eigenvalue_bounds=(0.0, 0.05**2))
         print('Loss:', loss.item())
         writer.add_scalar("Loss/min_eigval", loss, i)
 

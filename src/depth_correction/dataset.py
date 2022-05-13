d6e8bfc2ebfb650fb0a0dc49617de7481e99c70b
Tomas Petricek <tpetricek@gmail.com>
2022-05-13 11:12:14 +0200

    Depth bias filter allows estimating normals if not available.

--- src/depth_correction/dataset.py
+++ src/depth_correction/dataset.py
@@ -600,17 +600,22 @@ class NoisyDepthDataset(ForwardingDataset):
 
 class DepthBiasDataset(ForwardingDataset):
 
-    def __init__(self, dataset, model=None):
+    def __init__(self, dataset, model=None, cfg: Config=None):
         super().__init__(dataset)
         self.model = model
+        self.cfg = cfg
 
+    @timing
     def modify_cloud(self, cloud):
         if self.model is not None:
             assert isinstance(self.model, BaseModel)
             dc = DepthCloud.from_structured_array(cloud)
             assert isinstance(dc, DepthCloud)
-            assert dc.normals is not None
-            dc.update_incidence_angles()
+            if dc.normals is None:
+                print('Estimating normals from data.')
+                dc.update_all(k=self.cfg.nn_k, r=self.cfg.nn_r)
+            else:
+                dc.update_incidence_angles()
             dc = self.model.inverse(dc)
             pts = dc.to_points().detach().numpy()
             cloud[['x', 'y', 'z']] = unstructured_to_structured(pts, names=['x', 'y', 'z'])
